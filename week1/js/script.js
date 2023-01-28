bf1369915cdb39e759935f0ee05bdf1808263c93
xynthiawan <xdw201205@gmail.com>
2023-01-27 22:15:41 -0500

    fixes

--- week1/js/script.js
+++ week1/js/script.js
@@ -1,64 +1,3 @@
-/*let computerWins = 0;
-let userWins = 0;
-
-function pickWinner(input, computerPlay) {
-  // Check if userPlay is a valid move
-  input = prompt("Enter rock, paper, or scissors:");
-  let userPlay = input;
-  
-  if (userPlay !== "rock" && userPlay !== "paper" && userPlay !== "scissors") {
-    console.log("Invalid user play. Please enter 'rock', 'paper', or 'scissors'.");
-    return;
-  }
-  // Check if computerPlay is a valid move
-  if (computerPlay !== "rock" && computerPlay !== "paper" && computerPlay !== "scissors") {
-    console.log("Invalid computer play. Please enter 'rock', 'paper', or 'scissors'.");
-    return;
-  }
-  // Check for tie
-  if (userPlay === computerPlay) {
-    console.log("It's a tie!");
-    return;
-  }
-  // Check for user win
-  switch (userPlay) {
-    case "rock":
-      if (computerPlay === "scissors") {
-        userWins++;
-        console.log(`You win! Your score is ${userWins}`);//template literals, you need to use backticks (`)
-      } else {
-        computerWins++;
-        console.log(`You lose! Computer score is ${computerWins}`);
-      }
-      break;
-    case "paper":
-      if (computerPlay === "rock") {
-        userWins++;
-        console.log(`You win! Your score is ${userWins}`);//template literals, you need to use backticks (`)
-      } else {
-        computerWins++;
-        console.log(`You lose! Computer score is ${computerWins}`);
-      }
-      break;
-    case "scissors": 
-      if (computerPlay === "paper") {
-        userWins++;
-        console.log(`You win! Your score is ${userWins}`);//template literals, you need to use backticks (`)
-      } else {
-        computerWins++;
-        console.log(`You lose! Computer score is ${computerWins}`);
-      }
-  let confirmed = confirm("Do you want to play again?");
-  while (confirm !== true){
-    prompt("Enter rock, paper, or scissors:");
-  }
-  }
-}
-
-let computerPlay = Math.random() < 0.33 ? "rock" : Math.random() < 0.66 ? "paper" : "scissors";
-console.log("You played " + userPlay + " and the computer played " + computerPlay + ".");
-pickWinner(userPlay, computerPlay);
-*/
 
 let playerScore = 0;
 let computerScore = 0;
@@ -127,3 +66,4 @@ while (true) {
     let computerPlay = Math.random() < 0.5 ? "rock" : Math.random() < 0.75 ? "paper" : "scissors";
     pickWinner(userPlay, computerPlay);
 }
+// provide prompt
